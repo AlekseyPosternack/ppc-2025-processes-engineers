e5a0f87a10bc73f0af53b62f8b72aa981e28fea5
Alexey Posternak <lockharn@student.21-school.ru>
2025-12-15 19:52:30 +0300

    fix_pre_commit

--- tasks/posternak_a_increase_contrast/mpi/include/ops_mpi.hpp
+++ tasks/posternak_a_increase_contrast/mpi/include/ops_mpi.hpp
@@ -21,10 +21,9 @@ class PosternakAIncreaseContrastMPI : public BaseTask {
   bool PostProcessingImpl() override;
 
   std::vector<unsigned char> ScatterInputData(int rank, int size, int data_len);
-  void FindGlobalMinMax(const std::vector<unsigned char> &proc_part, unsigned char *data_min,
-                               unsigned char *data_max);
+  void FindGlobalMinMax(const std::vector<unsigned char> &proc_part, unsigned char *data_min, unsigned char *data_max);
   std::vector<unsigned char> ApplyContrast(const std::vector<unsigned char> &proc_part, unsigned char data_min,
-                                                  unsigned char data_max);
+                                           unsigned char data_max);
 };
 
 }  // namespace posternak_a_increase_contrast
